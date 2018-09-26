635497a67780566803da88ddb48c93a40d191775
Hart <hartx8@ex.ua>
2018-09-26 14:25:22 +0300

    before deleting controller

---
+++ spec/policies/FOR_EXAMPLE_policy_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+
+#EXAMPLE
+describe ArticlePolicy do
+  subject { described_class.new(user, article) }
+
+  let(:article) { Article.create }
+
+  context 'being a visitor' do
+    let(:user) { nil }
+
+    it { is_expected.to permit_action(:show) }
+    it { is_expected.to forbid_action(:destroy) }
+  end
+
+  context 'being an administrator' do
+    let(:user) { User.create(administrator: true) }
+
+    it { is_expected.to permit_actions([:show, :destroy]) }
+  end
+end
