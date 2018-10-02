384383a607b95d1174f6125210e03b7ccf6d58bd
Hart <hartx8@ex.ua>
2018-10-02 16:54:40 +0300

    add smtp to prodaction

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,19 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  config.action_mailer.default_url_options = { host: 'olegproject.herokuapp.com/' }
+
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      user_name:      'olegproject123',
+      password:       'gfhjkmgfhjkm123.',
+      domain:         'google.com',
+      address:       'smtp.gmail.com',
+      port:          '587',
+      authentication: :plain,
+      enable_starttls_auto: true
+    }
+
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
